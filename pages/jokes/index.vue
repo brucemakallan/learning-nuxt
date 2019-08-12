a65df20efe921e7ffbd909b9d00ebf841533aa43
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-08-12 14:25:19 +0300

    Load jokes on the jokes page/route

--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -2,11 +2,19 @@
   <div class="about-page">
     <h1>This is a learning application that delivers corny Dad Jokes</h1>
     <p>This project is written in Vue JS and used to learn Nuxt</p>
+    <ol>
+      <li v-for="(joke, index) in jokes" :key="index">{{ joke.joke }}</li>
+    </ol>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
+const API = "https://icanhazdadjoke.com/search";
+
 export default {
+  name: "Jokes",
   head() {
     return {
       title: "Jokes",
@@ -18,6 +26,22 @@ export default {
         }
       ]
     };
+  },
+  data() {
+    return {
+      jokes: []
+    };
+  },
+  async created() {
+    const config = {
+      headers: {
+        Accept: "application/json"
+      }
+    };
+    await axios
+      .get(API, config)
+      .then(res => (this.jokes = res.data.results))
+      .catch(error => console.log(error.data));
   }
 };
 </script>
